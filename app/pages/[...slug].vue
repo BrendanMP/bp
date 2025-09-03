c5d8691f93ec350da2c28ca76ef5d9c60dc4e901
brendanpete <brendan.pete@telrite.com>
2025-09-02 20:06:48 -0400

    basic content

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+const route = useRoute()
+const slug = computed(() => route.params.slug ? (route.params.slug as string[]).join('/') : '')
+
+const { data: content } = await useAsyncData(`content-${slug.value}`, () =>
+  queryCollection('content')
+    .path(`/${slug.value}` || '/')
+    .first())
+
+useSeoMeta({
+  title: content.value?.title,
+  description: content.value?.description,
+})
+</script>
+
 <template>
   <div>
-    {{ $route.params.slug }}
+    <nav>
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      <NuxtLink to="/about">
+        About
+      </NuxtLink>
+    </nav>
+    <ContentRenderer v-if="content" :value="content" />
+    <div v-else>
+      Page not found
+    </div>
   </div>
 </template>
